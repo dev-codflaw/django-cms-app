ba5df2a716ed483b672643c2c39b66460a1963aa
Umesh <dev.codflaw@gmail.com>
2021-03-10 22:42:06 +0530

    add media config in settings.py

--- config/settings.py
+++ config/settings.py
@@ -134,3 +134,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'staticfiles'),
 )
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
